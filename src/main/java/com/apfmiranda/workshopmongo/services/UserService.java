bac05bf19b6b65da32bd99d6d57043639b699f8e
apfmiranda <alexandre.miranda@gmail.com>
2019-05-04 11:41:44 -0300

    Obtendo um usuário por id

--- src/main/java/com/apfmiranda/workshopmongo/services/UserService.java
+++ src/main/java/com/apfmiranda/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.apfmiranda.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.apfmiranda.workshopmongo.domain.User;
 import com.apfmiranda.workshopmongo.repository.UserRepository;
+import com.apfmiranda.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return this.repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
