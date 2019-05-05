e10df8e1f4ed1fc22285251055536c0f693e4a15
apfmiranda <alexandre.miranda@gmail.com>
2019-05-05 17:48:56 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/apfmiranda/workshopmongo/config/Instantiation.java
+++ src/main/java/com/apfmiranda/workshopmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.apfmiranda.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.apfmiranda.workshopmongo.domain.Post;
 import com.apfmiranda.workshopmongo.domain.User;
+import com.apfmiranda.workshopmongo.dto.AuthorDTO;
+import com.apfmiranda.workshopmongo.repository.PostRepository;
 import com.apfmiranda.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,11 +19,18 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepositry;
+	
+	@Autowired
+	private PostRepository postReposiroty;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepositry.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -26,6 +38,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepositry.saveAll(Arrays.asList(maria ,alex, bob));
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		
+//		maria.getPosts().addAll(Arrays.asList(post1, post2));
+//		userReposiroty.save(maria);
 	}
 
 }
